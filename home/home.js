e98fb0549c0210bf5811855981759cff1dec52ab
Lynn <annelinde.huisman@gmail.com>
2025-07-14 13:29:26 +0000

    home start

---
+++ home/home.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+const naam = localStorage.getItem("gebruikersnaam");
+  if (naam) {
+    document.getElementById("naam-placeholder").textContent = naam;
+  } else {
+    document.getElementById("naam-placeholder").textContent = "gast";
+  }
